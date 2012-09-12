e71af094cfb303756ed1d930ceeed04062296250
nils <nils@376bb805-d709-4451-8b19-f41d6d19b301>
2012-09-12 17:44:09 +0000

    adding robust algorithm to calculate the intersectiongraph

--- src/main.cc
+++ src/main.cc
@@ -17,36 +17,83 @@ main()
 {
 	//printf("it has started....\n");
 	DataFile df("/home/nils/triforce/dev/dataConvex.dat",Binary);
-	Data3D *a;
-	a = df.digestBinaryTable();
+	Data3D *dat;
+	dat = df.digest3DBinaryTable();
 	
 	DataFile df2("/home/nils/seawater/nonpolar/fluorene.top",MapCSV);
-	Topology *b;
-	b = df2.digestTOP();
+	Topology *top;
+	top = df2.digestTOP();
 	
 	//b->print();
 	
 	DataFile df3("/home/nils/seawater/nonpolar/fluorene.gro",MapCSV);
-	Molecule *c;
-	c = df3.digestGRO(*b);
+	Molecule *mol;
+	//mol = df3.digestGRO(*top);
+	mol = new Molecule(*top);
+
+	/*
+	//testcase 0 (single occlusion)
+	mol->addRealAtom(0,-1.5,0,string("C1"));
+	mol->addRealAtom(0,1.3,0.5,string("C1"));
+	mol->addRealAtom(0,1.3,-0.5,string("C1"));
+	mol->addRealAtom(1,1.3,-0.5,string("C1"));
+	*/
+	
+	
+	//testcase 1 (outside / inside)
+	mol->addRealAtom(0,-1.5,0,string("C1"));
+	mol->addRealAtom(0,1.3,0.5,string("C1"));
+	mol->addRealAtom(0,1.3,-0.5,string("C1"));
+	mol->addRealAtom(0,1.3,-1.5,string("C1"));
+	
+	
+	/*
+	//testcase 2 (both)
+	mol->addRealAtom(0,-1.5,0,string("C1"));
+	mol->addRealAtom(0,1.3,0.75,string("C1"));
+	mol->addRealAtom(0,1.3,-0.75,string("C1"));
+	mol->addRealAtom(0,1.2,0,string("C1"));
+	*/
+
+	/*
+	//testcase 3 (totally inside)
+	mol->addRealAtom(0,-1.5,0,string("C1"));
+	mol->addRealAtom(0,1.3,1,string("C1"));
+	mol->addRealAtom(0,1.3,-1,string("C1"));
+	mol->addRealAtom(0,1.85,0,string("C1"));
+	*/
+	
+	/*
+	//testcase 4 (singles)
+	mol->addRealAtom(0,-1.5,0,string("C1"));
+	mol->addRealAtom(1.5,1.3,1,string("C1"));
+	mol->addRealAtom(1.5,1.3,-1,string("C1"));
+	mol->addRealAtom(-1.5,1.3,0,string("C1"));
+	*/
+	
+	/*
+	//testcase 4 (triforce)
+	mol->addRealAtom(0,-1.5,0,string("C1"));
+	mol->addRealAtom(1.2,1.3,1,string("C1"));
+	mol->addRealAtom(1.2,1.3,-1,string("C1"));
+	mol->addRealAtom(-1.2,1.3,0,string("C1"));
+	*/
+	
+	//mol->print();
 	
 	//c->print();
 	
-	Interpolation *interpolator = new Interpolation(a);
+	Interpolation interpolator(dat);
 	
-	int res=100;
-	double x[res];
-	double y[res];
-	Vector vec(3);
-	for(int i=0;i<res;++i){
-		double v = M_PI * (double)i/(double)res;
-		vec(0)=0.6;
-		vec(1)=v;
-		vec(2)=0.5;
-		y[i]=interpolator->multiPointTaylor(vec);
-		x[i]=v;
-		printf("%f %f\n",x[i],y[i]);
-	}
+	Tessellation tessellation(*mol);
+	tessellation.build();
+	string s("gbonnet.csv");
+	tessellation.outputGaussBonnetData(s);
+	
+	Integrator integrator(&tessellation, &interpolator, mol);
+	double area = integrator.integrate();
+	
+	printf("total area of molecule: %f\n",area);
 	
 	
 }
