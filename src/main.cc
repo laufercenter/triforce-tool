af4d49c1ecaaf1c39d3f5983520117b83c6fa035
nils <nils@376bb805-d709-4451-8b19-f41d6d19b301>
2013-02-14 18:25:36 +0000

    added calculation of numerical areas and derivatives, as well as comparison between triforce and numerical values by choice

--- src/main.cc
+++ src/main.cc
@@ -12,10 +12,34 @@ namespace po = boost::program_options;
 
 
 
-Molecule *mol;
+Molecule *mol, *mol_numerical;
 string libraryPath, top, gro;
 bool error;
-double area;
+double area, area_numerical;
+int numericalDetail;
+double numericalDifference;
+int compare;
+
+
+
+void progressbar(double percentage){
+	int size=80;
+	
+	cout << "\r" "[";
+	for(int i=0; i<size; ++i){
+		if((double)i/(double)(size-1) <= percentage)
+			cout << "=";
+		else
+			cout << " ";
+	}
+	cout << "] ";
+	cout << ((int)ceil(percentage*100));
+	cout << "%";
+	cout << std::flush;
+	
+	if(percentage>=1) cout << endl;
+	
+}
 
 
 int main(int ac, char* av[])
@@ -26,7 +50,10 @@ int main(int ac, char* av[])
 			("help,h", "help")
 			("library_path,l", po::value<string>(&libraryPath), "set library path")
 			("topology,t", po::value<string>(&top), "set topology file")
-			("structure,s", po::value<string>(&gro), "set structure file");
+			("structure,s", po::value<string>(&gro), "set structure file")
+			("numerical_detail,n", po::value<int>(&numericalDetail)->default_value(0), "numerical detail (default off)")
+			("numerical_difference,d", po::value<double>(&numericalDifference)->default_value(0), "numerical difference (default off)")
+			("compare,c", po::value<int>(&compare)->default_value(0), "show only semi-analytical/numerical difference (default off)");
 			
 		po::variables_map vm;
 		po::store(po::parse_command_line(ac,av,desc), vm);
@@ -49,7 +76,7 @@ int main(int ac, char* av[])
 			error=true;
 		}
 		if(!vm.count("structure")){
-			fprintf(stderr, "structure not set\n");
+			fprintf(stderr, "structure file not set\n");
 			error=true;
 		}
 		
@@ -76,10 +103,30 @@ int main(int ac, char* av[])
 
 	TriforceInterface trii(libraryPath);
 	double area = trii.calculateSurfaceArea(*mol);
-
-	mol->print();
 	
-	printf("TOTAL AREA: %f\n",area);
+	if(compare==0){
+		mol->print();
+		printf("TOTAL AREA: %f\n",area);
+	}
+	
+	if(numericalDetail>0){
+		Molecule *mol_numerical;
+		mol_numerical = dfgro.digestGRO(*top);
+		
+		IntegratorNumerical integratorNumerical(numericalDetail,numericalDifference);
+		area_numerical = integratorNumerical.integrate(mol_numerical, -1, &progressbar);
+		
+		if(compare==0){
+			mol_numerical->print();
+			printf("TOTAL AREA: %f\n",area_numerical);
+		}
+		else{
+			mol->printDifference(mol_numerical);
+			printf("TOTAL AREA: %f\n",area - area_numerical);
+		}
+	}
+
+	
 	
 	
 }
